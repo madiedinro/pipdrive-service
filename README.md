7cec674146ebc00750009b91c4a98f13e0b9413b
bolt.rstat.org <hello@rstat.org>
2018-12-05 23:53:28 +0000

    re-implemented

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Load persons, deals, stages, etc... Reload data when webhook received.
 
+
+```
+{'id': 9064, 'key': 'f699f454bfc8aaa65063207da2a4ddd89bd4c640', 'name': 'Google', 'order_nr': 26, 'field_type': 'varchar', 'add_time': '2018-09-15 15:03:04', 'update_time': '2018-11-18 14:45:16', 'active_flag': True, 'edit_flag': True, 'index_visible_flag': True, 'details_visible_flag': True, 'add_visible_flag': True, 'important_flag': True, 'bulk_edit_allowed': True, 'searchable_flag': True, 'filtering_allowed': True, 'sortable_flag': True, 'mandatory_flag': False}
+{'id': 9063, 'key': '5cc799445f8401d92d2f40030900228916b0921d', 'name': 'Telegram', 'order_nr': 27, 'picklist_data': None, 'field_type': 'varchar', 'add_time': '2018-09-12 18:12:06', 'update_time': '2018-11-04 23:21:35', 'active_flag': True, 'edit_flag': True, 'index_visible_flag': True, 'details_visible_flag': True, 'add_visible_flag': True, 'important_flag': True, 'bulk_edit_allowed': True, 'searchable_flag': True, 'filtering_allowed': True, 'sortable_flag': True, 'mandatory_flag': False}
+```
+
+
 ## License
 
 ```
--- pipedrive/main.py
+++ pipedrive/main.py
@@ -6,22 +6,43 @@ Band service skeleton
 import asyncio
 from itertools import count
 from prodict import Prodict as pdict
-from band import expose, worker, logger, settings
+from band import expose, worker, logger, settings, scheduler
+from pprint import pprint
 from .helpers import clean_phone
 from .pipedrive import pd_query, pd_post
 
 
-google_field = 'f699f454bfc8aaa65063207da2a4ddd89bd4c640'
-telegram_field = '5cc799445f8401d92d2f40030900228916b0921d'
-deal_course_field = 'ecc840eafa7ae6c76047f69218d7325610aee2d1'
+class state:
+    persons = pdict()
+    stages = pdict()
+    deals = pdict()
+    person_fields = pdict()
+    deal_fields = pdict()
 
-state = pdict(
-    persons=pdict(),
-    stages=pdict(),
-    person_fields=pdict(),
-    courses=pdict(),
-    deal_fields=pdict()
-)
+
+def prepare_field(field):
+    res = {
+        'key': field['name'].lower()
+    }
+    if 'options' in field:
+        res['options'] = {str(f['id']): f['label'] for f in field['options']}
+    return res
+
+
+def fix_struct(item, fields):
+    for k, val in [*item.items()]:
+        if len(k) == 40 and k in fields:
+            field = fields[k]
+            fk = field['key']
+            item[fk] = val
+            if 'options' in field and val:
+                item[fk] = field['options'][str(val)]
+    return item
+
+@expose()
+async def filter_persons(criteria):
+    field, val = [*criteria, None, None][:2]
+    return uniform_filter(state.persons, field, val)
 
 
 @expose()
@@ -31,6 +52,13 @@ async def course_persons(course):
         if course in person.courses:
             persons.append(person)
     return persons
+
+
+@expose()
+async def get_person(criteria):
+    field, val = criteria
+    persons = uniform_filter(state.persons, field, val)
+    return persons[0] if len(persons) else None
 
 
 def uniform_filter(items, field=None, val=None):
@@ -43,7 +71,7 @@ def uniform_filter(items, field=None, val=None):
             prop = item.get(field, None)
             print(item, prop, val)
             if prop is not None:
-                if isinstance(prop, list):
+                if isinstance(prop, list) or isinstance(prop, set):
                     if val in prop:
                         results.append(item)
                 else:
@@ -52,65 +80,51 @@ def uniform_filter(items, field=None, val=None):
     return results
 
 
-@expose()
-async def filter_persons(criteria):
-
-    field, val = [*criteria, None, None][:2]
-    return uniform_filter(state.persons, field, val)
-
-
-@expose()
-async def get_person(criteria):
-    field, val = criteria
-    for person in state.persons.values():
-        if person.get(field, None) == val and val is not None:
-            return person
-
-
-async def get_person_fields():
+async def load_person_fields():
     res = await pd_query('personFields')
     if res and 'data' in res:
-        for i in res['data']:
-            state.person_fields[i['key']] = i['name']
+        state.person_fields = {i['key'].lower(): prepare_field(i) for i in res['data'] if len(i['key']) == 40}
 
 
-async def get_deal_fields():
+async def load_deal_fields():
     res = await pd_query('dealFields')
     if res and 'data' in res:
-        for field in res['data']:
-            state.deal_fields[field['name']] = field['key']
-            if field['key'] == deal_course_field:
-                state.courses = {str(f['id']): f['label'] for f in field['options']}
+        state.deal_fields = {i['key'].lower(): prepare_field(i) for i in res['data'] if len(i['key']) == 40}
 
 
-async def get_persons():
-    persons = pdict()
-    res = await pd_query('persons')
-    if res and 'data' in res:
-        for p in res['data']:
-            person = format_person(p)
-            persons[person.id] = person
-        state.persons.clear()
-        state.persons.update(persons)
-
-
-async def get_stages():
+async def load_stages():
     res = await pd_query('stages')
     if res and 'data' in res:
+        state.stages.clear()
         for stage in res['data']:
             state.stages[stage['id']] = dict(pipeline_name=stage['pipeline_name'], name=stage['name'], id=stage['id'])
 
 
-async def get_deals():
+async def load_persons():
+    res = await pd_query('persons', {'limit': 500})
+    if res and 'data' in res:
+        state.persons.clear()
+        for p in res['data']:
+            pers_id = str(p['id'])
+            state.persons[pers_id] = format_person(fix_struct(p, state.person_fields))
+
+
+async def load_deals():
     res = await pd_query('deals', {'limit': 500})
     if res and 'data' in res:
+        state.deals.clear()
         for deal in res['data']:
-            if deal[deal_course_field]:
-                course_id = str(deal[deal_course_field])
-                person_id = str(deal['person_id']['value'])
-                if course_id and person_id and deal['person_id']['value']:
-                    course = state.courses.get(course_id)
-                    state.persons[person_id].courses.append(course)
+            pers_id = deal['person_id']['value'] = str(deal['person_id']['value'])
+            state.deals[deal['id']] = fix_struct(deal, state.deal_fields)
+            if deal.get('course', None) and deal.get('status', None) in ['open', 'won']:
+                state.persons[pers_id].courses.add(deal['course'])
+        logger.warn('deal', deal)
+            # if deal[deal_course_field]:
+            #     course_id = str(deal[deal_course_field])
+            #     person_id = str(deal['person_id']['value'])
+            #     if course_id and person_id and deal['person_id']['value']:
+            #         course = state.courses.get(course_id)
+            #         state.persons[person_id].courses.append(course)
 
 
 @expose()
@@ -119,7 +133,11 @@ async def create_deal(data, **params):
 
 
 def format_person(person):
-    view = pdict(name=person['name'], courses=[], email='', phone='', phone_part='', google=person[google_field], id=str(person['id']))
+    # view = pdict(name=person['name'], courses=[], email='', phone='', phone_part='', google=person[google_field], id=str(person['id']))
+    view = pdict(name=person['name'], courses=set(), email='', phone='', phone_part='', id=str(person['id']))
+
+    for field in ['google', 'telegram']:
+        view[field] = person[field]
 
     if len(person['phone']) and person['phone'][0]['value']:
         view.phone = clean_phone(person['phone'][0]['value'])
@@ -131,23 +149,36 @@ def format_person(person):
 
 
 async def reload_data():
-    await get_stages()
-    await get_person_fields()
-    await get_deal_fields()
-    await get_persons()
-    await get_deals()
+    await load_stages()
+    await load_person_fields()
+    await load_deal_fields()
+    await load_persons()
+    await load_deals()
 
 
-@expose.handler()
-async def deal(projectId, **params):
-    if projectId == settings.projectIdKey:
-        await reload_data()
+def check_key(params, settings):
+    pid_input = str(params.get('projectId', ''))
+    pid_config = str(settings.get('projectIdKey', ''))
+    pids_eq = pid_input == pid_config
+    if not pids_eq:
+        logger.debug(f'projectId not match: {pid_config}!={pid_input}', st=str(type(pid_config)), inp=str(type(pid_input)))
+    return pid_config and pids_eq
 
 
-@expose.handler()
-async def person(projectId, **params):
-    if projectId == settings.projectIdKey:
-        await reload_data()
+# https://bolt.rstat.org/pipedrive/deal/X
+# https://bolt.rstat.org/pipedrive/person/X
+@expose.handler(timeout=15000)
+async def deal(**params):
+    if check_key(params, settings):
+        await scheduler.spawn(reload_data())
+        return {'success': 1}
+
+
+@expose.handler(timeout=15000)
+async def person(**params):
+    if check_key(params, settings):
+        await scheduler.spawn(reload_data())
+        return {'success': 1}
 
 
 @worker()
